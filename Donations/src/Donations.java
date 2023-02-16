7808246369d208792f270ca7fee3012188eade1b
jsonfrench <114097230+jsonfrench@users.noreply.github.com>
2023-02-16 16:05:37 -0500

    Add files via upload

---
+++ Donations/src/Donations.java
@@ -0,0 +1,19 @@
+
+public class Donations {
+	
+	private int numIndividual;
+	private float amtIndividual;
+	private int numBusiness;
+	private int numOther;
+	private float amtOther;
+
+	public void processDonation(String cat, double donation) {
+		;
+	}
+	
+	public void getStatistics() {
+		System.out.print("Number of donations for: (category)\n");
+		System.out.print("Total donations for: (amount)\n");
+	}
+
+}
---
+++ Donations/src/DonationsTester.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DonationsTester {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File donations_txt = new File("Donations.txt");	//import .txt file
+		Scanner donations_reader = new Scanner(donations_txt);
+		
+		Scanner input = new Scanner(System.in);		//import scanner
+		Donations donate = new Donations();			//import donations class
+		
+		System.out.print("Would you like to process donations now? (enter \'Yes\' to continue):");
+		
+		String user_response = input.nextLine();	//read input
+		user_response = user_response.toLowerCase();
+		boolean response_is_yes = user_response.equals("yes");
+		
+		if (!response_is_yes) {						//guard cause
+			System.out.print("Ending now without processing donations\n");
+			System.exit(0);
+		}
+		
+		//read from file
+		String line;
+		while (donations_reader.hasNextLine()) {
+		line = donations_reader.nextLine();
+		System.out.println(line);
+		}
+	}
+
+}
