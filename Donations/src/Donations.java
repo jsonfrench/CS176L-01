2f561402710324279a00dc2dea35798567a5f745
jsonfrench <114097230+jsonfrench@users.noreply.github.com>
2023-02-20 14:56:15 -0500

    Add files via upload

--- Donations/src/Donations.java
+++ Donations/src/Donations.java
@@ -4,6 +4,7 @@ public class Donations {
 	private int numIndividual;
 	private float amtIndividual;
 	private int numBusiness;
+	private float amtBusiness;
 	private int numOther;
 	private float amtOther;
 
@@ -12,8 +13,12 @@ public class Donations {
 	}
 	
 	public void getStatistics() {
-		System.out.print("Number of donations for: (category)\n");
-		System.out.print("Total donations for: (amount)\n");
+		System.out.print("Number of donations for individual: " + numIndividual + "\n");
+		System.out.print("Total donations for individual: " + amtIndividual + "\n");
+		System.out.print("Number of donations for business: " + numBusiness + "\n");
+		System.out.print("Total donations for business: " + amtBusiness + "\n");
+		System.out.print("Number of donations for other: " + numOther + "\n");
+		System.out.print("Total donations for other: " + amtOther + "\n");
 	}
 
 }
--- Donations/src/DonationsTester.java
+++ Donations/src/DonationsTester.java
@@ -9,16 +9,16 @@ public class DonationsTester {
 		File donations_txt = new File("Donations.txt");	//import .txt file
 		Scanner donations_reader = new Scanner(donations_txt);
 		
-		Scanner input = new Scanner(System.in);		//import scanner
-		Donations donate = new Donations();			//import donations class
+		Scanner input = new Scanner(System.in);			//import scanner
+		Donations giveToMe = new Donations();			//import donations class
 		
 		System.out.print("Would you like to process donations now? (enter \'Yes\' to continue):");
 		
-		String user_response = input.nextLine();	//read input
+		String user_response = input.nextLine();		//read input
 		user_response = user_response.toLowerCase();
 		boolean response_is_yes = user_response.equals("yes");
 		
-		if (!response_is_yes) {						//guard cause
+		if (!response_is_yes) {							//guard cause
 			System.out.print("Ending now without processing donations\n");
 			System.exit(0);
 		}
@@ -27,8 +27,20 @@ public class DonationsTester {
 		String line;
 		while (donations_reader.hasNextLine()) {
 		line = donations_reader.nextLine();
-		System.out.println(line);
+
+		String[] parsed_line = line.split(">");
+		int donation_amount = Integer.parseInt(parsed_line[1]);
+
+		System.out.println(parsed_line.length);
+		
+		if (parsed_line.length > 1) {
+			System.out.println(donation_amount);
 		}
+		
+		}
+
+		giveToMe.getStatistics();
+
 	}
 
 }
