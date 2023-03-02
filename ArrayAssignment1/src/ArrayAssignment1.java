9b152edab0c768c4800924f72c8e47aeab3e7867
jsonfrench <114097230+jsonfrench@users.noreply.github.com>
2023-03-02 15:04:16 -0500

    Add files via upload

--- ArrayAssignment1/src/ArrayAssignment1.java
+++ ArrayAssignment1/src/ArrayAssignment1.java
@@ -8,7 +8,35 @@ public class ArrayAssignment1 {
 		for(double value : values) {
 			System.out.printf("%s ", value);
 		}
+		System.out.print("\n");
 		
+		double largest = 0;
+		for(double value : values) {
+			if(value > largest) {
+				largest = value;				
+			}
+		}
+		System.out.printf("The largest element in the array is %s%n", largest);
+		
+		int negative = 0;
+		for(double value : values) {
+			if(value < 0) {
+				negative += 1;				
+			}
+		}
+		System.out.printf("There are %s negative numbers in the array%n", negative);
+		
+
+	
+
+
+/*		
+		for (int i = 0; i < values.length; i++) {
+			System.out.println(values[i]);;
+		}
+*/
+
+
 	}
 
 }
