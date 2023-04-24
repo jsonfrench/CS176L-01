f1730096796a12c73ac51c51e78027d45e73e088
jsonfrench <114097230+jsonfrench@users.noreply.github.com>
2023-04-24 15:40:19 -0400

    Add files via upload

--- Fangraph Jsoup project/src/FangraphTopTen.java
+++ Fangraph Jsoup project/src/FangraphTopTen.java
@@ -1,55 +1,91 @@
-import org.jsoup.Jsoup;
+import java.io.*;
+import java.util.ArrayList;
+
+import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
-import java.util.*;
+
 
 public class FangraphTopTen {
-    public static void main(String[] args) {
-        // Store the base URL of Fangraphs
-        final String BASE_URL = "https://www.fangraphs.com";
-        
-        // Store the URLs for the batting leaderboards for the years 2022, 2021, and 2020
-        String[] urls = {
-            "/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2022&month=0&season1=2022&ind=0&team=0&rost=0&age=0&filter=&players=0",
-            "/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2021&month=0&season1=2021&ind=0&team=0&rost=0&age=0&filter=&players=0",
-            "/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2020&month=0&season1=2020&ind=0&team=0&rost=0&age=0&filter=&players=0"
-        };
-        
-        // Store the list of players for each year
-        List<String>[] playerLists = new ArrayList[3];
-        for (int i = 0; i < 3; i++) {
-            playerLists[i] = new ArrayList<>();
-        }
-        
-        // Scrape the player names from each leaderboard and add them to the corresponding player list
-        for (int i = 0; i < 3; i++) {
-            try {
-                Document doc = Jsoup.connect(BASE_URL + urls[i]).get();
-                Elements players = doc.select("td.player > a");
-                for (Element player : players) {
-                    playerLists[i].add(player.text());
-                }
-            } catch (IOException e) {
-                System.out.println("Error connecting to Fangraphs: " + e.getMessage());
-            }
-        }
-        
-        // Combine the player lists and count the occurrences of each player
-        Map<String, Integer> playerCounts = new HashMap<>();
-        for (List<String> playerList : playerLists) {
-            for (String player : playerList) {
-                playerCounts.put(player, playerCounts.getOrDefault(player, 0) + 1);
-            }
-        }
-        
-        // Sort the player counts in descending order and print the top ten players
-        List<Map.Entry<String, Integer>> sortedPlayers = new ArrayList<>(playerCounts.entrySet());
-        sortedPlayers.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        System.out.println("Top ten batting leaders for 2022, 2021, and 2020:");
-        for (int i = 0; i < 10; i++) {
-            System.out.println((i + 1) + ". " + sortedPlayers.get(i).getKey() + " (" + sortedPlayers.get(i).getValue() + " times)");
-        }
-    }
+	
+	public static void main(String args[]) throws IOException {
+		
+		final int TOP_NUM = 10;
+		int year = 2020;
+		
+		ArrayList<String> twenty = new ArrayList();
+		
+		Document doc = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2020&month=0&season1=2020&ind=0&team=0&rost=0&age=0&filter=&players=0")
+				.timeout(6000).get();
+		
+		Elements body = doc.select("tbody");
+		int counter = 0;
+		
+		for (Element e: body.select("td.grid_line_regular")) {
+			String name = e.select("a").text();
+			if (name != "" && name.length() > 3) {
+				twenty.add(name);
+				
+			}
+		}
+		
+		System.out.printf("%s:%n", year);
+		for (int i = 0; i < TOP_NUM; i++) {
+			System.out.printf("%s: %s%n", i+1, twenty.get(i));
+		}
+		System.out.printf("%n", null);		
+		
+		
+		
+		ArrayList<String> twentyone = new ArrayList();
+		
+		Document doc_one = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2021&month=0&season1=2021&ind=0&team=0&rost=0&age=0&filter=&players=0")
+				.timeout(6000).get();
+		
+		Elements body_one = doc_one.select("tbody");
+		counter = 0;
+		
+		for (Element e: body_one.select("td.grid_line_regular")) {
+			String name = e.select("a").text();
+			if (name != "" && name.length() > 3) {
+				twentyone.add(name);
+				
+			}
+		}
+		
+		System.out.printf("%s:%n", year+1);
+		for (int i = 0; i < TOP_NUM; i++) {
+			System.out.printf("%s: %s%n", i+1, twentyone.get(i));
+		}
+		System.out.printf("%n", null);		
+	
+		
+			
+		ArrayList<String> twentytwo = new ArrayList();
+		
+		Document doc_two = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2022&month=0&season1=2022&ind=0&team=0&rost=0&age=0&filter=&players=0")
+				.timeout(6000).get();
+		
+		Elements body_two = doc_two.select("tbody");
+		counter = 0;
+		
+		for (Element e: body_two.select("td.grid_line_regular")) {
+			String name = e.select("a").text();
+			if (name != "" && name.length() > 3) {
+				twentytwo.add(name);
+				
+			}
+		}
+		
+		System.out.printf("%s:%n", year+2);
+		for (int i = 0; i < TOP_NUM; i++) {
+			System.out.printf("%s: %s%n", i+1, twentytwo.get(i));
+		}
+		System.out.printf("%n", null);		
+	
+		
+			
+		
+	}
 }
